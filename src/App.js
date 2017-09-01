69457702c368cdd88c7896dcb24c78652c7024b8
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2017-09-01 07:03:07 -0500

    added logout feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  state={ loggedIn: false }
 
   componentWillMount() {
     firebase.initializeApp({
@@ -15,13 +17,28 @@ class App extends Component {
     storageBucket: 'authentication-de57d.appspot.com',
     messagingSenderId: '946899066268'
   });
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      this.setState({ loggedIn: true });
+    } else {
+      this.setState({ loggedIn: false });
+    }
+  });
 }
+
+  renderContent() {
+    if (this.state.loggedIn) {
+      return <Button>Log out</Button>;
+    }
+      return <LoginForm />;
+  }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -12,16 +12,26 @@ class LoginForm extends Component {
 
     this.setState({ error: '', loading: true });
 
-      firebase.auth().signInWithEmailAndPassword(email, password)
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
       .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          .then(() => {
-            this.setState({ error: 'Suck Sex' });
-          })
-          .catch(() => {
-            this.setState({ error: 'Authentication Failed' });
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
           });
-      });
+  }
+
+  onLoginFail() {
+    this.setState({ error: 'Authintication Fail', loading: false })
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    });
   }
 
   renderButton() {
